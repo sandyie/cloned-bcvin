fb870becbc7ba571e7dcba9eb45ed38a678ed98b
Faith Jones <faj@st-andrews.ac.uk>
2019-12-05 15:54:42 -0800

    function now seems to be working

--- hardiness/analyses/ColumnCF_function.R
+++ hardiness/analyses/ColumnCF_function.R
@@ -2,7 +2,11 @@
 
 
 adjustcf <- function(hisData, cd, ce, year, month, day, doynum, period){
+	yearDates <- data.frame(matrix(NA,length(unique(year)), 3))
+	names(yearDates ) <- c("Year", "dateAcc", "dateMax")
+	yearDates$Year <- unique(year)
 	adjustcf <- c()
+	
 	
 	# this function replicates the column CF coding in Car's hardiness spreadsheet
 	# input:
@@ -15,51 +19,50 @@ adjustcf <- function(hisData, cd, ce, year, month, day, doynum, period){
 	# doynum - this is the day of teh year the data comes from
 	# period - this is what hardiness period the data is in 
 
+	#hisData  <- climall$meanC2day.hist
+	#ce <- climall$CE 
+	#cd <- climall$CD
+	#year <- climall$Year
+	#month <- climall$month
+	#day <- climall$day
+	#doynum <- climall$doynum
+	#period <- climall$HardinessPeriod
+
+	#geting the days of the year for the different sections of the rest of teh function
+	#this needs doing because of leap years  
 
 
+	for (yeari in unique(year)){
 
-
-head(climall)
-
-#what day of the year is October 31st? This is the day that CF starts to decline from 1
-climall$doynum[climall$month == "Oct" & climall$day == 29]
-
-#for 2012 
-
-climall2012 <- climall[climall$Year == 2012,]
-
-	changeDateAcc <- climall2012 $doynum[climall2012$month == "Oct" & climall2012 $day == "30"]
-	startDateMax <- climall2012 $doynum[climall2012$month == "Dec" & climall2012 $day == "08"]
+		yearDates$dateAcc[yearDates$Year == yeari] <- doynum[month == "Oct" & day == 29 & year == yeari]
+		yearDates$dateMax[yearDates$Year == yeari] <- doynum[month == "Dec" & day == 8 & year == yeari]
+	}
 
 	for(i in c(1:length(hisData)))
 		{
-		if(is.na(period[i]) == TRUE) {
-				adjustcf [i] <- NA
-			} 
+		if( is.na(period[i]) == TRUE) {adjustcf [i] <- NA
+		} else if (period[i] == "Acc" & doynum[i] <= yearDates$dateAcc[yearDates$Year == year[i]] ){adjustcf [i] <- 1
+		} else if (period[i] == "Acc" & !day[i] > yearDates$dateAcc[yearDates$Year == year[i]] ){adjustcf [i] <- adjustcf [i-1] - 0.007
+		} else if (doynum[i]  == yearDates$dateMax[yearDates$Year == year[i]]){adjustcf [i] <- 1.3
+		} else if (period[i] == "Max" & !doynum[i]  == yearDates$dateMax[yearDates$Year == year[i]] ){adjustcf [i] <- adjustcf [i-1] - 0.01
+		} else if (period [i] == "Max2") {adjustcf [i] <- adjustcf [i-1] - 0.01
+		#for Deacc there is an equation =IF(AND(BY165>3,CD165<0),0.1,(CD165*CE165))BY is teh average 2 day mean 
+		#historical temperature data climall$meanC2day.hist
 
 
-#if the day of the year is more than  303 (changeDate) and in teh "Acc" hardiness period then the value for 
-#cf shoudl be 1
-	
-	else if (period[i] == "Acc" & day < changeDateAcc ){
-		adjustcf [i] <- 1
-			}
+		} else 
+			if(hisData [i]> 3 & cd[i]< 0) {adjustcf [i] <- 0.1
+				} else adjustcf [i] <- cd[i]*ce[i]
 
-#after this date the vlaue shoudl decline by 0.007 until december the 7th (end of accumilation period)
+		}
 
-	else if (period[i] == "Acc" & !day < changeDateAcc ){
-		adjustcf [i] <- adjustcf [i-1] - 0.007
-			}
 
-#in teh maximum accumilation period the values starts at 1.30 and declines by 0.01 until feb teh 7th when
-#Deacc starts 
-
+	return(adjustcf)
+}
 
 
 
 
-#for Deacc there is an equation =IF(AND(BY165>3,CD165<0),0.1,(CD165*CE165)). BY is teh average 2 day mean 
-#historical temperature data climall$meanC2day.hist
 
 
 
