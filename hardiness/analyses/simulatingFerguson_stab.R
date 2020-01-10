7b592ba5f0de69a615901c305f3349e10fdcce8f
FaithJones <faithann.jones2@gmail.com>
2020-01-10 14:34:16 -0800

    simulating the data seems ok

--- hardiness/analyses/simulatingFerguson_stab.R
+++ hardiness/analyses/simulatingFerguson_stab.R
@@ -1,27 +1,33 @@
 #Simulating_hardiness from Ferguson et al 2011, but using my own temperature data
 
+rm(list = ls())
 setwd("/home/faith/Documents/github/bcvin/hardiness/analyses")
 
-tempData <- read.csv("input/climhist_19812010.csv")
+#tempData <- read.csv("input/climhist_19812010.csv")#Carls historical temp data
+tempData <- read.csv("input/DummyTempChard.csv")#temp data scraped from Fergusun 2011 figure of chardonay grape temps
 
-head(tempData)
 
-#temperature values 
-temps <- tempData$X2day
-plot(temps)
+#getting temperature data for each day 
+fit1 <- lm(temperature ~ poly(Month, 4, raw = TRUE), data = tempData)# get a model that fits the data (mostly)
+plot(tempData$temperature ~ tempData$Month)
+lines(tempData$Month, predict(fit1, data.frame( x = tempData$Month)), col="red")
+day <- 7/221 # number of months/number of dates 
+simDays <- data.frame(Month = seq(1,8, day))
+simDays$simTemps <- predict(fit1, newdata = simDays)
+
 
 # a list of parameters needed 
 #-------------------------------
 
-i <- 2 # dont start on 1 becuase then the follwoing asymptotic (clogs) bounds wont work 
-tempValues <- temps# a set temperature values for the winter. i used real data. 
+i1 <- 2 # dont start on 1 becuase then the follwoing asymptotic (clogs) bounds wont work 
+tempValues <- tempData$simTemps # a set temperature values for the winter. I used data from the original manuscript  
 
 #other parameters setimated by the model
 hcInit <- -10.55 # start hardiness value. mean of earliest LTE50 in the fall 
 hcMax <- -25.24 # maximum hardiness
 hcMin <- -3 # min hardiness. Hardiness of green tissues 
 tThreshEnd <- 11.75 # threshold temperature for accumilating cold during endodormancy 
-tThreshEct <- 3.75 # threshold for accumilating worming during ectodormancy
+tThreshEct <- 3.75 # threshold for accumilating warming during ectodormancy
 kaEnd <- 0.14 # constant accumilation rate during endodormancy
 kaEct <- 0.07 # constant accumilation rate during ectodormancy
 kdEnd <- 0.01 # constant deaccumilation rate during endodormancy
@@ -29,12 +35,12 @@ kdEct <- 0.13 # constant deaccumilation during ectodormancy
 edb <- -750 # amount of chilling required to switch from endo to ecto dormancy 
 
 hcChange <- vector()
-hcChange[i-1] <- NA
+hcChange[i1-1] <- NA
 hc <- vector()
-hc[i-1] <- hcInit
+hc[i1-1] <- hcInit
 
 #initiating secondary parameters
-cloga <- vector()#the correction putting absolute bounds on change in hc. Equations 5 and 6 of Fergusonetal2011
+cloga <- vector() #the correction putting absolute bounds on change in hc. Equations 5 and 6 of Fergusonetal2011
 clogd <- vector()
 
 #tempeperatures below the mean threshold values are chilling days, and above are heating
@@ -43,29 +49,29 @@ ddEct <- tempValues - tThreshEct
 
 #make a vector of chilling days only 
 ddEndc <- ddEnd
-ddEndc[ddEndc > tThreshEnd ] <- 0
+ddEndc[ddEndc > 0 ] <- 0
+
 #ddEndc <- ddEndc * -1 # i think these should be negative values?
 
 ddEctc <- ddEct
-ddEctc[ddEctc > tThreshEct ] <- 0
+ddEctc[ddEctc > 0 ] <- 0
 #ddEctc <- ddEctc * -1
 
 #vector of heating days only 
 ddEndh <- ddEnd
-ddEndh[ddEndh < tThreshEnd] <- 0
+ddEndh[ddEndh < 0] <- 0
 
 ddEcth <- ddEct
-ddEcth[ddEcth < tThreshEct] <- 0
+ddEcth[ddEcth < 0] <- 0
 
 #get accumilating degree growing days
 accumilatedDD <- cumsum(ddEndc)
 
 #main model for Endodormancy for teh first value i = 2
 #------------------------------------
-
-cloga[i] <- 1 - ((hcMin - hc[i-1])/(hcMin-hcMax))
-clogd[i] <- 1 - ((hc[i-1]-hcMax)/(hcMin - hcMax))
-hcChange[i] <- (ddEndc[i] * kaEnd * cloga[i])+(ddEndh[i] * kdEnd* clogd[i])
+#cloga[i] <- 1 - ((hcMin - hc[i-1])/(hcMin-hcMax))
+#clogd[i] <- 1 - ((hc[i-1]-hcMax)/(hcMin - hcMax))
+#hcChange[i] <- (ddEndc[i] * kaEnd * cloga[i])+(ddEndh[i] * kdEnd* clogd[i])
 
 #build a loop for all data 
 
@@ -94,4 +100,5 @@ for (i in (2:length(tempValues))){
 plot(hcChange)
 plot(hc)
 
-accumilatedDD > edb
+
+
