edc87c80a6b095a54ca9967e4d0be56d4208f559
FaithJones <faithann.jones2@gmail.com>
2020-01-10 16:50:25 -0800

    adding a simple linear model and plots

---
+++ hardiness/analyses/LinearModelHardiness.R
@@ -0,0 +1,122 @@
+rm(list = ls())
+#script started by Faith Jones on the 10th Jan 2020 
+
+#running a simpel linear model of hardiness againts temperature for chardonay
+#using Carl's data
+
+setwd("/home/faith/Documents/github/bcvin/hardiness/analyses/")
+
+
+#libraries
+#install.packages("reshape2")
+library(reshape2)
+
+#climate data
+clim <- read.delim("input/envcanada_penticton.csv", skip=25, sep=",", header=TRUE)
+clim$date <- as.Date(clim$Date.Time, format="%m/%d/%y")
+clim$month <- format(clim$date, "%b")
+clim$day<- format(clim$date,"%d")
+head(clim)
+climsm <- subset(clim, select=c("Year", "month","day", "Mean.Temp..C.", "Mean.Temp.Flag", "date", "Date.Time"))
+names(climsm) <- c("Year", "month","day", "meanC", "meanC.flag", "date", "Date.Time")
+
+# hardiness data
+#--------------------------
+budhardiness2012to13 <- read.csv("input/budhardiness2012to13.csv", header=TRUE)
+budhardiness2013to14 <- read.csv("input/budhardiness2013to14.csv", header=TRUE)
+budhardiness2014to15 <- read.csv("input/budhardiness2014to15.csv", header=TRUE)
+budhardiness2015to16 <- read.csv("input/budhardiness2015to16.csv", header=TRUE)
+budhardiness2016to17 <- read.csv("input/budhardiness2016to17.csv", header=TRUE)
+budhardiness2017to18 <- read.csv("input/budhardiness2017to18.csv", header=TRUE) 
+budhardiness2018to19 <- read.csv("input/budhardiness2018to19.csv", header=TRUE) 
+
+bh12 <- melt(budhardiness2012to13, id.var=c("X2012...2013", "Variety"))
+bh13 <- melt(budhardiness2013to14, id.var=c("X2013...2014", "Variety"))
+bh14 <- melt(budhardiness2014to15, id.var=c("X2014...2015", "Variety"))
+bh15 <- melt(budhardiness2015to16, id.var=c("X2015...2016", "Variety"))
+bh16 <- melt(budhardiness2016to17, id.var=c("site", "Variety")) 
+bh17 <- melt(budhardiness2017to18, id.var=c("site", "X2017...2018")) 
+bh18 <- melt(budhardiness2018to19, id.var=c("site", "Variety")) 
+
+nameshere <- c("site", "variety", "Date", "lte")
+names(bh12) <- nameshere
+names(bh13) <- nameshere
+names(bh14) <- nameshere
+names(bh15) <- nameshere
+names(bh16) <- nameshere
+names(bh17) <- nameshere
+names(bh18) <- nameshere
+bh12$years <- "2012to2013"
+bh13$years <- "2013to2014"
+bh14$years <- "2014to2015"
+bh15$years <- "2015to2016"
+bh16$years <- "2016to2017"
+bh17$years <- "2017to2018"
+bh18$years <- "2018to2019"
+
+bhall.rbind <- rbind(bh12, bh13, bh14, bh15, bh16, bh17, bh18)
+
+# remove the averages....
+bhall <- subset(bhall.rbind, site!="Average Bud Hardiness (all sites, all varieties)")
+
+# cleaning names
+sort(unique(bhall$site))
+bhall$site[bhall$site=="Naramata bench"] <- "Naramata Bench"
+sort(unique(bhall$variety))
+sort(unique(bhall$Date))
+# cleaning dates
+breakbyperiod <- strsplit(as.character(bhall$Date), ".", fixed=TRUE) 
+bhall$Day <- unlist(lapply(breakbyperiod, function(x) x[1]))
+bhall$month <- unlist(lapply(breakbyperiod, function(x) x[2]))
+bhall$day <- unlist(lapply(strsplit(as.character(bhall$Day), "X", fixed=TRUE), function(x) x[2]))
+
+# right, so now, we need to fix year!
+bhall$year <- NA
+bhall$year[bhall$years=="2012to2013" & (bhall$month=="Oct"|bhall$month=="Nov"|bhall$month=="Dec")] <- 2012
+bhall$year[bhall$years=="2012to2013" & (bhall$month=="Jan"|bhall$month=="Feb"|bhall$month=="Mar"|bhall$month=="Apr")] <- 2013
+bhall$year[bhall$years=="2013to2014" & (bhall$month=="Oct"|bhall$month=="Nov"|bhall$month=="Dec")] <- 2013
+bhall$year[bhall$years=="2013to2014" & (bhall$month=="Jan"|bhall$month=="Feb"|bhall$month=="Mar"|bhall$month=="Apr")] <- 2014
+bhall$year[bhall$years=="2014to2015" & (bhall$month=="Oct"|bhall$month=="Nov"|bhall$month=="Dec")] <- 2014
+bhall$year[bhall$years=="2014to2015" & (bhall$month=="Jan"|bhall$month=="Feb"|bhall$month=="Mar"|bhall$month=="Apr")] <- 2015
+bhall$year[bhall$years=="2015to2016" & (bhall$month=="Oct"|bhall$month=="Nov"|bhall$month=="Dec")] <- 2015
+bhall$year[bhall$years=="2015to2016" & (bhall$month=="Jan"|bhall$month=="Feb"|bhall$month=="Mar"|bhall$month=="Apr")] <- 2016
+bhall$year[bhall$years=="2016to2017" & (bhall$month=="Oct"|bhall$month=="Nov"|bhall$month=="Dec")] <- 2016
+bhall$year[bhall$years=="2016to2017" & (bhall$month=="Jan"|bhall$month=="Feb"|bhall$month=="Mar"|bhall$month=="Apr")] <- 2017
+bhall$year[bhall$years=="2017to2018" & (bhall$month=="Oct"|bhall$month=="Nov"|bhall$month=="Dec")] <- 2017
+bhall$year[bhall$years=="2017to2018" & (bhall$month=="Jan"|bhall$month=="Feb"|bhall$month=="Mar"|bhall$month=="Apr")] <- 2018
+bhall$year[bhall$years=="2018to2019" & (bhall$month=="Oct"|bhall$month=="Nov"|bhall$month=="Dec")] <- 2018
+bhall$year[bhall$years=="2018to2019" & (bhall$month=="Jan"|bhall$month=="Feb"|bhall$month=="Mar"|bhall$month=="Apr")] <- 2019
+
+# and make a useful df
+bh <- subset(bhall, select=c("year", "month", "day", "variety", "lte", "site"))
+head(bh)
+
+#make a date column 
+bh$Date <- paste(bh$month, bh$day, bh$year, sep = "/")
+bh$Datestrptime <- as.POSIXct(strptime(bh$Date ,format="%b/%d/%Y"))
+
+#select only one year's data 
+climsm$Datestrptime <- as.POSIXct(strptime(climsm$Date.Time,format="%m/%d/%y"))
+#note, dates that are in strptime format rather than as,POSIXct cannot be used to merge 
+
+#combine datasets
+bhclim <- merge(bh, climsm, by.x = "Datestrptime", by.y = "Datestrptime") 
+head(bhclim)
+str(bhclim)
+plot(bhclim$lte ~ bhclim$meanC)
+
+#simple lm
+
+lmFit <- lm(lte ~ meanC, data = bhclim)
+plot(bhclim$lte ~ bhclim$meanC)
+abline(lmFit, col = "red")
+
+lmFit2 <- lm(lte ~ year, data = bhclim)
+summary(lmFit2)
+boxplot(bhclim$lte ~ bhclim$year)
+
+lmFit3 <- lm(lte ~ variety, data = bhclim)
+summary(lmFit3)
+boxplot(bhclim$lte ~ bhclim$variety)
+
+
