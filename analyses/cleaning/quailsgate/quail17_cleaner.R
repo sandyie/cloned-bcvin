2faceb378d6357c3bdf484c734de02fb72088c4f
Mira Garner <mira.garner@gmail.com>
2020-10-06 18:32:31 -0700

    updates

--- analyses/cleaning/quailsgate/quail17_cleaner.R
+++ analyses/cleaning/quailsgate/quail17_cleaner.R
@@ -1,26 +1,59 @@
 #cleaning script for Quails Gate Phenology data 2017-2019 (September 2019, Mira Garner)
+
 rm(list = ls())
 options(stringsAsFactors = FALSE)
-
-setwd("~/Documents/git/bcvin/analyses/input")
-quail17 <- read.csv("~/Documents/git/bcvin/analyses/input/qg_phenology2017_19COPY.csv")
 
 library(lubridate)
 library(dplyr)
 
-# add column for variety - NEED TO DO BEFORE SPLIT
-#### need to look at maps and Asset Identifier, create another spreadsheet of the asset.id with variety then match with asset.id in pheno spreadsheet?
+setwd("~/Documents/git/bcvin/analyses/input/quailsgate/")
+q <- read.csv("qg_phenology2017_19COPY.csv")
+mp <- read.csv("VineyardMapsDataCompiled.csv")
+
+head(q)
+head(mp)
+
+# remove lines that are not observations - look at again, some pheno info in Other.obs?
+notphen  <- q[which(q$Asset.Class == "Admin / Staff"),] #putting them in df to look at
+q <- q[which(q$Asset.Class != "Admin / Staff"),] #removing them from dataset
+
+# asplit Asset.Indentifier column into block and vneyard columns - rought split since the format isn't the same for all entries. Will need to clean up some vineyard names and block names. Maybe Faith or Geoff has more elegant method?
+assetsplit <- strsplit(q$Asset.Identifier, " ", fixed = TRUE, useBytes = TRUE)
+q$block <- unlist(lapply(assetsplit, function(x) x[1]))
+q$vineyard <- unlist(lapply(assetsplit, function(x) x[2]))
+
+# fix vineyard names
+q$vineyard[which(q$vineyard == "Estate")] <- "QuailsGateEstate"
+q$vineyard[which(q$vineyard == "Blue")] <- "BlueFox"
+q$vineyard[which(q$vineyard == "Stewart")] <- "StewartFamilyEstates"
+q$vineyard[which(q$vineyard == "West")] <- "WestPoint"
+
+q$vineyard[which(q$vineyard == "\x95" & q$block == "Beaumont")] <- "Beaumont"
+q$block[which(q$vineyard == "Beaumont" & q$block == "Beaumont")] <- "1-0"
+q$vineyard[which(q$vineyard == "\x95" & q$block == "Banducci")] <- "Banducci"
+q$block[which(q$vineyard == "Banducci" & q$block == "Banducci")] <- "0"
+q$block[which(q$vineyard == "")] <- "0"
+q$vineyard[which(q$vineyard == "")] <- "Illig"
 
 
-# add column for vineyard - split from block
-# add column for block - weird block/vineyard names??)
+#q[which(q$vineyard == "&"),] <- "WestPoint" #fix block numbers first
+
+
+q <- q[which(!is.na(q$vineyard)),]
+
+
 #<<DATA>>$Asset.Identifier <- sub(" ", "_", <<DATA>>$Asset.Identifier) #to replace space
+
+# add column for variety
+
+# won't have variety match spreadsheet for vineyards: Patricia, WestPoint, Illig, StewartFamilyEstates, Beaumont, Banducci,
 
 # split date into columns: year, month, day
 ###Question: which date is right?? Diff btwn TaskDate and DateOfObs
-<<DF>>$year <- year(mdy(<<DF>>$<DATE>))
-<<DF>>$month <- month(mdy(<<DF>>$<<DATE>>))
-<<DF>>$day <- day(mdy(<<DF>>$<<DATE>>))
+breakdate <- strsplit(as.character(q$Date.of.Observation), "-", fixed=TRUE)
+q$day <- unlist(lapply(breakdate, function(x) x[1]))
+q$month <- unlist(lapply(breakdate, function(x) x[2]))
+q$year <- unlist(lapply(breakdate, function(x) x[3]))
 
 # add column for event - what do stages mean?
 
@@ -29,13 +62,10 @@ library(dplyr)
 
 # add column for company: Quails Gate
 company <- "QuailsGate"
-NEXTDATA <- cbind(<<DATA>>, company)
+q2 <- cbind(q2, company)
 
 # add notes column
-#Other.observations <- notes
-#library(data.table)
-#setnames(data, old=c("old_name","another_old_name"), new=c("new_name", "another_new_name"))
-# OR colnames(data)[colnames(data)=="old_name"] <- "new_name" for single columns
+colnames(q2)[colnames(q2) == "Other.observations"] <- "notes"
 
 # reorder columns
-clean.quail17 <- select(<<DATA>>, c("company", "vineyard", "block", "variety", "year", "month", "day", "event", "value", "notes"))
+clean.quail17 <- select(q2, c("company", "vineyard", "block", "variety", "year", "month", "day", "event", "value", "notes"))
--- analyses/input/_README.txt
+++ analyses/input/_README.txt
@@ -15,7 +15,7 @@ The input folder contains files copied into .csv's from the bcvin/data folder. S
 > qg_PhenoDataReport_2012-2016.csv copied from "Phenological Data Report 2012 - 2016.xlsx"
 > qg_2017_PreHarvestGrapeAnalysis.csv copied from "2017 Pre-Harvest Grape Analysis.xlsx"
 > input/quailsgate/phenologicaldata - contains spreadsheets copied from individual sheets (tabs) in "PHENOLOGICAL DATA.xls" (these sheets were edited by Mika Y so see "PHENOLOGICAL DATA_ed_MY.xls" for direct match)
-> input/quailsgate/vineyardmaps - contains VineyardMapsDataCompiled.csv which is information transcribed by Mika Y from the pdf maps in data/quailsgate/vineyardmaps
+> VineyardMapsDataCompiled.csv which is information transcribed by Mika Y from the pdf maps in data/quailsgate/vineyardmaps
 > input/quailsgate/viticulturesummary - contains clones_ed_MY.csv. This folder is still in progress. All relevant information from the VITICULTURE SUMMARY.xls sheets should go in here.
 
 >> Still need the following files or folders to be copied to input:
